86811a5f933158fddacd87d4c0c7025b9a4c947d
Felipe Renan <feliperenanvieira@gmail.com>
2020-07-25 19:05:55 -0300

    Adding Slim Framework 4

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Hello world";
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello Slim Framework 4!");
+    return $response;
+});
+
+$app->run();
