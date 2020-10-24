9f290bc11796195f809bcc562479ae06e239816c
Felipe Vieira <feliperenanvieira@gmail.com>
2020-10-24 03:12:18 -0300

    Removing unused use.

--- src/Http/Controller/JsonController.php
+++ src/Http/Controller/JsonController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controller;
 
-use App\Service\Hello\IHelloService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
