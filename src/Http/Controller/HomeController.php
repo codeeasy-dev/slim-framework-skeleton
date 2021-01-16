4e2754f5bd3b146fc0df0d954a99318aea6a5293
Felipe Vieira <feliperenanvieira@gmail.com>
2021-01-16 19:36:55 -0300

    Updating HomeController twig usage

--- src/Http/Controller/HomeController.php
+++ src/Http/Controller/HomeController.php
@@ -8,22 +8,22 @@ use App\Service\Responder\IResponderService;
 use App\Service\Twig\ITwigService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Twig\Environment;
 
 class HomeController
 {
-    private ITwigService $twig;
+    private Environment $twig;
     private IResponderService $responder;
 
     public function __construct(ITwigService $twig, IResponderService $responder)
     {
-        $this->twig = $twig;
+        $this->twig = $twig->buildTwigObject();
         $this->responder = $responder;
     }
 
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         $body = $this->twig
-            ->buildTwigObject()
             ->render(
                 'home/index.twig',
                 ['message' => 'Hello World!!!']
